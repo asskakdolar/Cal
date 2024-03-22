7f79d37090c5b9c0ef9e9240ce452f517432fda3
asskakdolar <lowlo@yandex.ru>
2024-03-22 11:06:52 +0300

    Update main.go

--- main.go
+++ main.go
@@ -15,26 +15,26 @@ func main() {
 	fmt.Println("Давай попробуем, возможности ты знаешь,просто потыкай =) ")
 	number, x, y, opr, eror := readLine()
 	if eror != nil {
-		fmt.Println("дангер.")
+		panic("дангер.")
 		fmt.Println(eror)
 		return
 	}
 	if number == "arab" {
 		numb_1, e_1 := strconv.Atoi(x)
 		if e_1 != nil {
-			fmt.Println("Дангер!")
+			panic("Дангер!Получилась Паника?")
 			fmt.Println(e_1)
 			return
 		}
 		numb_2, e_2 := strconv.Atoi(y)
 		if e_2 != nil {
-			fmt.Println("Дангер!")
+			panic("Дангер!")
 			fmt.Println(e_2)
 			return
 		}
 		ans, e_3 := calculator(numb_1, numb_2, opr)
 		if e_3 != nil {
-			fmt.Println("Дангер!")
+			panic("Дангер!")
 			fmt.Println(e_3)
 			return
 		} else {
@@ -46,13 +46,13 @@ func main() {
 		numb_2 := fromRomanToInt(y)
 		ans, e_1 := calculator(numb_1, numb_2, opr)
 		if e_1 != nil {
-			fmt.Println("Дангер!")
+			panic("Дангер!")
 			fmt.Println(e_1)
 			return
 		} else {
 			end, e_2 := fromIntToRoman(ans)
 			if e_2 != nil {
-				fmt.Println("Что то поломалось")
+				panic("Что то поломалось")
 				fmt.Println(e_2)
 				return
 			}
@@ -64,6 +64,8 @@ func main() {
 
 func calculator(numb_1 int, numb_2 int, opr string) (int, error) {
 	if numb_1 > 10 || numb_2 > 10 {
+
+		panic("ой ой ой,да что же такое.")
 		return 7, errorHandler(7)
 	}
 	switch {
@@ -85,6 +87,8 @@ func readLine() (string, string, string, string, error) {
 	usInput = strings.TrimSpace(usInput)
 	number, numb_1, numb_2, opr, eror := checkInput(usInput)
 	if eror != nil {
+
+		panic("Скорее всего я не правильно понял интерфейс паника,и пременяю его не правильно.")
 		return "", "", "", "", eror
 	}
 	return number, numb_1, numb_2, opr, eror
@@ -100,16 +104,19 @@ func checkInput(input string) (string, string, string, string, error) {
 		isS := isSign(value)
 		isR := isRomanNumber(value)
 		if !isN && !isS && !isR {
+			panic("ого паника паника?")
 			return "", "", "", "", errorHandler(1)
 		}
 		if isS {
 			if opr != "" {
+				panic("паника ,все ведь правильно у меня получаетс?")
 				return "", "", "", "", errorHandler(5)
 			} else {
 				opr = arr[index]
 			}
 		}
 		if (isN && number != "roman") || (isR && number != "arab") {
+			panic("ну если  программа не ругается,и нет красненькой ошибки,то пишим дальше. Кстаит чуть не забыл. Паника")
 			if number == "" {
 				if isN {
 					number = "arab"
@@ -118,6 +125,7 @@ func checkInput(input string) (string, string, string, string, error) {
 				}
 			}
 			if numb_1 == "" && !(index+1 == len(arr)) && isSign(arr[index+1]) {
+				panic("Паника!")
 				slice := arr[0:(index + 1)]
 				numb_1 = strings.Join(slice, "")
 			} else if index+1 == len(arr) && numb_1 != "" {
@@ -129,6 +137,8 @@ func checkInput(input string) (string, string, string, string, error) {
 		}
 	}
 	if numb_2 == "" || numb_1 == "" || opr == "" {
+
+		panic("ой ой ой,паника!")
 		return "", "", "", "", errorHandler(3)
 	}
 	return number, numb_1, numb_2, opr, nil
@@ -136,6 +146,7 @@ func checkInput(input string) (string, string, string, string, error) {
 
 func isNumber(c string) bool {
 	if c >= "0" && c <= "9" {
+		panic("Паника,она самая.")
 		return true
 	} else {
 		return false
@@ -144,6 +155,7 @@ func isNumber(c string) bool {
 
 func isSign(c string) bool {
 	if c == "+" || c == "-" || c == "/" || c == "*" {
+		panic("Паника! Вот и все,приплыли...")
 		return true
 	} else {
 		return false
@@ -152,6 +164,7 @@ func isSign(c string) bool {
 func isRomanNumber(c string) bool {
 	_, ok := dict[c]
 	if ok {
+		panic("Паника! Я определенно делаю что то не то!")
 		return true
 	} else {
 		return false
@@ -193,6 +206,7 @@ func fromRomanToInt(roman string) int {
 	arr := strings.Split(roman, "")
 	for index, value := range arr {
 		if index+1 != len(arr) && dict[value] < dict[arr[index+1]] {
+			panic("Паника!")
 			res -= dict[value]
 		} else {
 			res += dict[value]
@@ -203,6 +217,8 @@ func fromRomanToInt(roman string) int {
 
 func fromIntToRoman(number int) (string, error) {
 	if number <= 0 {
+
+		panic("Паника,я жее ее отследил? А интерфес рекавер нужен тут?")
 		return "", errorHandler(6)
 	}
 	arr1 := [13]int{1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1}
@@ -211,6 +227,7 @@ func fromIntToRoman(number int) (string, error) {
 	for number > 0 {
 		for i := 0; i < 13; i++ {
 			if arr1[i] <= number {
+				panic("Паника! Растет число желтеньких восклицательных знаков,что бы это могло значить?")
 				str += arr2[i]
 				number -= arr1[i]
 				break
